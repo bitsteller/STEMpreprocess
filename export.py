301d55301ff215a73f848189d85f17adee5c89d6
Nils Breyer <nbnet@arcor.de>
2015-07-09 16:34:03 -0700

    fix export to csv

--- export.py
+++ export.py
@@ -21,13 +21,13 @@ if __name__ == '__main__':
 		raise IOError(config.TRIPS_PATCHED_FILE + " already exists.")
 
 	f = open(config.TRIPS_PATCHED_FILE, 'a')
-	f.write("agent_id,commute_direction,orig_TAZ,dest_TAZ,cells_ID_string")
+	f.write("agent_id,commute_direction,orig_TAZ,dest_TAZ,cells_ID_string\n")
 	for i, (agent_id, commute_direction, orig_taz, dest_taz, cellpath) in enumerate(mcur):
 		if i % 10000 == 1:
 			est = datetime.datetime.now() + datetime.timedelta(seconds = (time.time()-start)/i*(count-i))
 			sys.stderr.write('\rdone {0:%}'.format(float(i)/count) + "  ETA " + est.strftime("%Y-%m-%d %H:%M"))
 		if cellpath != None:
-			f.write("{0},{1},{2:.1f},{3:.1f},{4}".format(agent_id, commute_direction, orig_taz, dest_taz, " ".join(map(str, cellpath))))
+			f.write("{0},{1},{2:.1f},{3:.1f},{4}\n".format(agent_id, commute_direction, orig_taz, dest_taz, " ".join(map(str, cellpath))))
 
 	f.close()
 	mconn.close()
